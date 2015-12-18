b721a065c4e068336ccf9c12ac3ec095f5d9a8d8
Martins Grunskis <mgrunskis@newstore.com>
2015-12-18 17:47:33 +0100

    new todo

--- load_data.sql
+++ load_data.sql
@@ -1,4 +1,5 @@
 delete from activities;
 
 insert into activities (description, points_done, points_total) values ('Reading Go book and doing exercises', 1, 13);
-insert into activities (description, points_done, points_total) values ('Reading Italian study book and doing exercises', 3, 20);
+insert into activities (description, points_done, points_total) values ('Reading Italian study book and doing exercises', 2, 15);
+insert into activities (description, points_done, points_total) values ('Reading Dolomiti 500 word book and write down new words', 12, 12);
